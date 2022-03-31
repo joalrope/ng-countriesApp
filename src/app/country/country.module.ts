d40e57e6e33ba804579a1a982c520ed4fc4e2e17
joalrope <joalrope@gmail.com>
2022-03-31 10:09:05 -0400

    First commit

---
+++ src/app/country/country.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
+import { ByCountryComponent } from './pages/by-country/by-country.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
+import { CountryInfoComponent } from './pages/country-info/country-info.component';
+
+@NgModule({
+  declarations: [
+    ByCapitalComponent, //
+    ByCountryComponent,
+    ByRegionComponent,
+    CountryInfoComponent,
+  ],
+  exports: [
+    ByCapitalComponent, //
+    ByCountryComponent,
+    ByRegionComponent,
+    CountryInfoComponent,
+  ],
+  imports: [
+    CommonModule, //
+  ],
+})
+export class CountryModule {}
---
+++ src/app/country/pages/country-info/country-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-info',
+  templateUrl: './country-info.component.html',
+  styles: [
+  ]
+})
+export class CountryInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
